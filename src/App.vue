645f87593bcdf6e9f0cf9916efb831c9f72408fc
gabriel matos <gabrielmatos@gabriels-MacBook-Air.local>
2023-03-09 22:27:48 -0300

    add: adicionando funcionanlidade principal do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      gitUserName: "",
+      profile: {},
+    };
+  },
+  methods: {
+    getGitProfile(username) {
+      fetch(`https://api.github.com/users/${username}`)
+        .then((res) => res.json())
+        .then((data) => {
+          this.profile = data;
+        });
+    },
+  },
+};
 </script>
 
 <template>
+  <input type="text" placeholder="Search Profile" v-model="gitUserName" />
+  <button @click="getGitProfile(gitUserName)">Search</button>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <ul>
+      <li v-for="(item, key) in profile">{{ key }}: {{ item }}</li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
